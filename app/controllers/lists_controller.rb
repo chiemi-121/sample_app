f5826bd605f803402e3bf1cdb3dd42242c28e708
Chiemi Takano <276151091+chiemi-121@users.noreply.github.com>
2026-05-10 06:36:33 +0000

    railscend

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,6 +1,6 @@
 class ListsController < ApplicationController
   def new
-   @list = List.new
+    @list = List.new
   end
 
   def create
@@ -8,41 +8,40 @@ class ListsController < ApplicationController
     if @list.save
       redirect_to list_path(@list.id)
     else
-      render :new, status: :unprocessable_entity
+      
+      render :new
     end
   end
-  
-  def show
-    @list = List.find(params[:id])  
-  end
 
+  def show
+    @list = List.find(params[:id])
+  end
 
   def index
     @lists = List.all
+    # index画面で投稿フォームも表示している場合は、空のインスタンスが必要
+    @list = List.new 
   end
 
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
-    redirect_to list_path(list.id)  
+    redirect_to list_path(list.id)
   end
-  
-  def destroy
-    list = List.find(params[:id])  # データ（レコード）を1件取得
-    list.destroy  # データ（レコード）を削除
-    redirect_to '/lists'  # 投稿一覧画面へリダイレクト  
 
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to lists_path # '/lists' より lists_path の方が安全です
   end
 
   private
 
   def list_params
-   params.require(:list).permit(:title, :body, :image)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
-
-
